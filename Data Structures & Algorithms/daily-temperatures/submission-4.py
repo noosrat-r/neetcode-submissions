e78e9814307a47f0f399b9e3c89c41b7748403f4
Noos Rahman <noosratrahman@outlook.com>
2026-06-30 22:53:40 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0]*len(temperatures)
+        for i,t in enumerate(temperatures):
+            while stack and stack[-1][0] < t:
+                temp, index = stack.pop()
+                res[index] = i - index
+            stack.append([t,i])
+
+        return res
