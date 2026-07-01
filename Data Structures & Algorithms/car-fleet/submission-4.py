137ae0bcdd22231803f00b3c52f70597a5ac6e08
Noos Rahman <noosratrahman@outlook.com>
2026-06-30 21:49:07 -0500

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # must sort to ensure order
+        # if not sorted, cant ensure the cars being checked dont have cars inbetween them
+        
+        # need to make a new array of position,speed pairs
+        # go through, ddo a calculation of (position - currPosition) / speed = time
+
+        # can use stack or not
+        cars = [(p,s) for p,s in zip(position,speed)]
+        cars.sort(reverse = True)
+
+        fleets = 1
+        prevTime = (target-cars[0][0])/cars[0][1]
+        for i in range(1,len(cars)):
+            currTime = (target-cars[i][0])/cars[i][1]
+
+            if currTime > prevTime:
+                prevTime = currTime
+                fleets += 1
+        return fleets
