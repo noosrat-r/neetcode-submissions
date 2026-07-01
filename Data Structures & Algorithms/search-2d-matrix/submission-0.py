20dceb721cef5be87861b4d038ce3a6e77371409
Noos Rahman <noosratrahman@outlook.com>
2026-06-30 22:11:44 -0500

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # m x n 2D array - matrix
+        # integer - target
+    
+        # each row is sorted in increasing order 
+        # first integer of row > last integer of prev row
+        # return true if target exists or false if does not exist
+
+        # aim for O(log(m*n))
+
+        # brute force is O(m*n) checking each row & each column 
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == target:
+                    return True
+
+        return False
