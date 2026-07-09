1f583c17d42dd1a360ba101f0aff075d9e6fe650
Noos <noosratrahman@outlook.com>
2026-07-08 23:01:18 -0500

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        res = -1
+        for i,n in enumerate(nums):
+            if n == target:
+                res = i
+        
+        return res
