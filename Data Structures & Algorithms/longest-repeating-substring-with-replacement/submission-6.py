336cdba3e5f65c2e97340536e5ad2673d2c768e7
Noos <noosratrahman@outlook.com>
2026-07-16 23:27:00 -0500

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,38 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # Note: problem specifies upper case english letter which is a hint to use a O(26) array to hold some info
+
+        # loop over every character, then loop over every character after it
+        # for every substring, calculate the max frequency
+        # only if ( r - l ) - maxfrequency <= k, then calculate the res
+
+        # res = 0
+        # for i in range(len(s)):
+        #     maxfreq = 0
+        #     countMap = defaultdict(int)
+        #     for j in range(i, len(s)):
+        #         countMap[s[j]] += 1
+        #         maxfreq = max(maxfreq, countMap[s[j]])
+        #         if (j - i + 1) - maxfreq <= k:
+        #             res = max(res, j - i + 1)
+        
+        # return res
+
+        res = 0
+        charSet = set(s)
+        for c in charSet:
+            count = 0
+            left = 0
+            for right in range(len(s)):
+                if s[right] == c:
+                    count += 1
+                
+                while (right-left+1)-count > k:
+                    if s[left] == c:
+                        count -= 1
+                    
+                    left += 1
+
+                res = max(res, right-left+1)
+
+        return res
